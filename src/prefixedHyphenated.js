3c7635b8edc26706c0ea82d0c1d839267302d77f
Stu Cox <stuart.cox@gmail.com>
2013-07-16 18:28:59 +0100

    Handled case of `prefixed()` returning false and added to `config-all.json`

--- src/prefixedHyphenated.js
+++ src/prefixedHyphenated.js
@@ -1,11 +1,12 @@
 define(['ModernizrProto', 'prefixed', 'domToHyphenated'], function( ModernizrProto, prefixed, domToHyphenated ) {
-    // Modernizr.prefixedHyphenated() returns the prefixed or nonprefixed property name variant of your input in hyphenated format
-    // Modernizr.prefixedHyphenated('transition') // '-moz-transition'
+  // Modernizr.prefixedHyphenated() returns the prefixed or nonprefixed property name variant of your input in hyphenated format
+  // Modernizr.prefixedHyphenated('transition') // '-moz-transition'
 
-    // Properties must be passed as dom-style camelcase.
-    // Return values will be the hyphenated variant
-    var prefixedHyphenated = ModernizrProto.prefixedHyphenated = function(prop) {
-        return domToHyphenated(prefixed(prop));
-    };
-    return prefixedHyphenated;
+  // Properties must be passed as dom-style camelcase.
+  // Return values will be the hyphenated variant
+  var prefixedHyphenated = ModernizrProto.prefixedHyphenated = function(prop) {
+    var prefixedProp = prefixed(prop);
+    return prefixedProp && domToHyphenated(prefixedProp);
+  };
+  return prefixedHyphenated;
 });
