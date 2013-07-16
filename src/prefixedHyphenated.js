1dd99f075d2e1a1a989b17d3c52f7b29e78838ec
Stu Cox <stuart.cox@gmail.com>
2013-07-16 18:11:49 +0100

    Added domToHyphenated dependency

--- src/prefixedHyphenated.js
+++ src/prefixedHyphenated.js
@@ -1,6 +1,6 @@
 define(['ModernizrProto', 'prefixed', 'domToHyphenated'], function( ModernizrProto, prefixed, domToHyphenated ) {
     // Modernizr.prefixedHyphenated() returns the prefixed or nonprefixed property name variant of your input in hyphenated format
-    // Modernizr.prefixedHyphenated('transition') // 'MozTransition'
+    // Modernizr.prefixedHyphenated('transition') // '-moz-transition'
 
     // Properties must be passed as dom-style camelcase.
     // Return values will be the hyphenated variant
