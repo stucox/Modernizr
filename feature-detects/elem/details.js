82023876c8f70a7cd63563adea0b8cbae5352574
Stu Cox <stuart.cox@gmail.com>
2014-03-07 10:37:56 +0000

    Converted `details` detect to object format

--- feature-detects/elem/details.js
+++ feature-detects/elem/details.js
@@ -11,25 +11,32 @@
   }]
 }
 !*/
-define(['Modernizr', 'createElement', 'docElement', 'testStyles'], function( Modernizr, createElement, docElement, testStyles ) {
-  Modernizr.addTest('details', function() {
-    var el = createElement('details');
-    var diff;
+define(['Modernizr', 'createElement', 'insertStyles'], function( Modernizr, createElement, insertStyles ) {
+  Modernizr.addTest('details', {
+    setUp: function ( elem ) {
+      var cssText = '#e! details{display:block}';
+      var details = createElement('details');
 
-    // return early if possible; thanks @aFarkas!
-    if (!('open' in el)) {
-      return false;
+      details.innerHTML = '<summary>a</summary>b';
+      elem.appendChild(details);
+
+      insertStyles(cssText, elem);
+    },
+    test: function ( elem ) {
+      var details = elem.children[0];
+      var height;
+
+      // return early if possible; thanks @aFarkas!
+      if (!('open' in details)) {
+        return false;
+      }
+
+      // Otherwise, we’re looking for a change in height when
+      // details is opened
+      // TODO: Can we do this without DOM read-write-read?
+      height = details.offsetHeight;
+      details.open = true;
+      return height != details.offsetHeight;
     }
-
-    testStyles('#modernizr details{display:block}', function( node ) {
-      node.appendChild(el);
-      el.innerHTML = '<summary>a</summary>b';
-      diff = el.offsetHeight;
-      el.open = true;
-      diff = diff != el.offsetHeight;
-    });
-
-
-    return diff;
   });
 });
